d818e08ce0a07db517586bca54bd3185de57ec74
OFFIAH ANTHONY1 <ebubeoffiah95@gmail.com>
2025-10-20 00:08:31 +0100

    update commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Desserts Variables
+
+const addWaffle = document.getElementById("add-waffle");
+const addCremeBrulee = document.getElementById("add-creme-brulee");
+const addMacaron = document.getElementById("add-macaron");
+const addTiramisu = document.getElementById("add-tiramisu");
+const addBaklava = document.getElementById("add-baklava");
+const addPie = document.getElementById("add-pie");
+const addCake = document.getElementById("add-cake");
+const addBrownie = document.getElementById("add-brownie");
+const addPannaCotta = document.getElementById("add-panna-cotta");
